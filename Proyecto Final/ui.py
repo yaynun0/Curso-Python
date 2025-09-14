92c825ab57e760c7c9cd4c11bd6095d72e97607a
yaynun0 <l.gerardonun@gmail.com>
2025-09-13 19:57:03 -0600

    Update ui.py

--- Proyecto Final/ui.py
+++ Proyecto Final/ui.py
@@ -1,10 +1,22 @@
 from tkinter import *
 
-class MainWindow(Tk):
+class CenterWidgetMixin:
+    def center(self):
+        self.update()
+        w=self.winfo_width()
+        h=self.winfo_height()
+        ws=self.winfo_screenwidth()
+        hs=self.winfo_screenheight()
+        x=int(ws/2-w/2)
+        y=int(hs/2-h/2)
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
         self.title("Gestor de clientes")
         self.build()
+        self.center()
 
     def build(self):
         button = Button(self,text="Hola", command=self.hola)
@@ -12,6 +24,7 @@ class MainWindow(Tk):
 
     def hola(self):
         print("Hola mundo!")
+
 
 if __name__=="__main__":
     app=MainWindow()
