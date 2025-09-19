9dc06b74179d8a02d16d7e03fcc17883358ecda5
yaynun0 <l.gerardonun@gmail.com>
2025-09-18 18:36:32 -0600

    Creacion y validacion

--- Proyecto Final/ui.py
+++ Proyecto Final/ui.py
@@ -1,6 +1,8 @@
+import helpers
 import database as db
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import askokcancel, WARNING
 
 
 class CenterWidgetMixin:
@@ -14,12 +16,69 @@ class CenterWidgetMixin:
         y=int(hs/2-h/2)
         self.geometry(f"{w}x{h}+{x}+{y}")
 
+
+class CreateClientWindow(Toplevel, CenterWidgetMixin):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Crear cliente")
+        self.build()
+        self.center()
+        self.transient(parent)
+        self.grab_set()
+
+    def build(self):
+        frame=Frame(self)
+        frame.pack(padx=20,pady=10)
+
+        Label(frame,text="DNI (2 ints y 1 upper char)").grid(row=0,column=0)
+        Label(frame,text="Nombre (2 a 30 chars)").grid(row=0,column=1)
+        Label(frame,text="Apellido (2 a 30 chars)").grid(row=0,column=2)
+
+        dni = Entry(frame)
+        dni.grid(row=1, column=0)
+        dni.bind("<KeyRelease>", lambda event: self.validate(event,0))
+
+        nombre = Entry(frame)
+        nombre.grid(row=1,column=1)
+        nombre.bind("<KeyRelease>", lambda event: self.validate(event,1))
+
+        apellido = Entry(frame)
+        apellido.grid(row=1,column=2)
+        apellido.bind("<KeyRelease>", lambda event: self.validate(event,2))
+
+        frame = Frame(self)
+        frame.pack(pady=10)
+
+        crear=Button(frame, text="Crear", command=self.create_client)
+        crear.configure(state=DISABLED)
+        crear.grid(row=0,column=0)
+        Button(frame, text="Cancelar",command=self.close).grid(row=0,column=1)
+
+
+    def create_client(self):
+        pass
+
+    def close(self):
+        self.destroy()
+        self.update()
+
+    def validate(self,event,index):
+        valor=event.widget.get()
+        valido = helpers.dni_valido(valor,db.Clientes.lista) if index==0 \
+            else (valor.isalpha() and len(valor)>=2 and len(valor) <=30)
+        event.widget.configure({"bg":"Green" if valido else "Red"})
+
+
+
+
+
 class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self):
         super().__init__()
         self.title("Gestor de clientes")
         self.build()
         self.center()
+        
 
     def build(self):
         frame = Frame(self)
@@ -53,11 +112,28 @@ class MainWindow(Tk, CenterWidgetMixin):
         frame = Frame(self)
         frame.pack(pady=20)
 
-        Button(frame,text="Crear",command=None).grid(row=0,column=0)
+        Button(frame,text="Crear",command=self.create).grid(row=0,column=0)
         Button(frame,text="Modificar",command=None).grid(row=0,column=1)
-        Button(frame,text="Borrar",command=None).grid(row=0,column=2)
+        Button(frame,text="Borrar",command=self.delete).grid(row=0,column=2)
 
         self.treeview = treeview
+
+    def delete(self):
+        cliente = self.treeview.focus()
+        if cliente:
+            campos = self.treeview.item(cliente, 'values')
+            confirmar = askokcancel(
+                title="Confirmar borrado",
+                message=f"Borrar {campos[1]} {campos[2]}?",
+                icon=WARNING
+            )
+            if confirmar:
+                self.treeview.delete(cliente)
+    
+    def create(self):
+        CreateClientWindow(self)
+
+    
 
 if __name__=="__main__":
     app=MainWindow()
